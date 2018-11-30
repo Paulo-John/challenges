d78056b2b1db370081b81f33516b3505781ace52
Paulo Jonathas <paulo.jonathas@hp.com>
2018-11-30 17:10:07 -0300

    challenge 11

--- 11/generators-challenge.py
+++ 11/generators-challenge.py
@@ -1,4 +1,5 @@
 import os
+from collections import Counter
 
 
 def gen_files(pat):
@@ -8,6 +9,23 @@ def gen_files(pat):
                 yield '{}/{}'.format(directory[0], filename)
 
 
+def gen_lines(files):
+    for file_path in files:
+        with open(file_path) as f:
+            for line in f.readlines():
+                yield line
+
+
+def gen_grep(lines, pattern):
+    for line in lines:
+        if line.startswith(pattern):
+            yield line
+
+
 if __name__ == '__main__':
-    for file in gen_files('..'):
-        print(file)
+    counter = Counter()
+    for i in gen_grep(gen_lines(gen_files('..')), 'import '):
+        counter[i.split()[1]] += 1
+
+    for v, c in sorted(counter.items(), key=lambda x: x[1], reverse=True):
+        print('      {} {}'.format(c, v))
